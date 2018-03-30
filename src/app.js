93c51eb973e6e6e9eeef52269d29a01c792b45cb
gogolab <m4ttek@gmail.com>
2018-03-30 19:18:06 +0200

    implemented getting post from json-server

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
-const greeting = 'Hello World';
-console.log(greeting);
+import { http } from "./http";
+import { ui } from "./ui";
+
+document.addEventListener("DOMContentLoaded", getPosts);
+
+function getPosts() {
+    http.get("http://localhost:3000/posts")
+        .then(data => ui.showPosts(data))
+        .catch(err => console.log(err));
+};
