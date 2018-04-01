1345b11f686757d7ac8c79c7d9dd0f7c4020691d
gogolab <m4ttek@gmail.com>
2018-04-02 00:24:38 +0200

    implemented adding posts

--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@ class UI {
     constructor() {
         this.titleInput = document.querySelector("#title");
         this.bodyInput = document.querySelector("#body");
-        this.post = document.querySelector("#post");
+        this.posts = document.querySelector("#posts");
         this.idInput = document.querySelector("#id");
         this.postSubmit = document.querySelector(".post-submit");
         this.formState = "add";
@@ -26,9 +26,42 @@ class UI {
                     </div>
                 </div>
             `;
-        })
+        });
 
-        this.post.innerHTML = output;
+        this.posts.innerHTML = output;
+    }
+
+    showAlert(message, className) {
+        this.clearAlert();
+
+        const div = document.createElement("div");
+
+        div.className = className;
+
+        div.appendChild(document.createTextNode(message));
+
+        const container = document.querySelector(".postsContainer");
+
+        container.insertBefore(div, this.posts);
+
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector(".alert");
+
+        console.log("current alert: ", currentAlert);
+
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = "";
+        this.bodyInput.value = "";
     }
 }
 
